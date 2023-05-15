7dbfdf7bfc191339f1111cfa6f4e04e6d4bbab83
jan.fojtik <jan.fojtik@indicia.nl>
2023-05-15 08:34:28 +0200

    idk what this even is anymore

--- research/mediapipe_pose.py
+++ research/mediapipe_pose.py
@@ -7,10 +7,10 @@ import cvzone as cvz
 import mediapipe as mp
 import numpy as np
 
+from PIL import ImageFont
+
 WIDTH = 1280
 HEIGHT = 960
-
-
 
 def pythagoras_normalized(landmarkA, landmarkB):
     return math.sqrt(
@@ -18,7 +18,7 @@ def pythagoras_normalized(landmarkA, landmarkB):
         + (landmarkA.y - landmarkB.y) ** 2
     )
 
-def drawLine(landmarkA, landmarkB, frame, wideBoyFactor, hex="FFFFFF"):
+def draw_line(landmarkA, landmarkB, frame, wideBoyFactor, hex="FFFFFF"):
     cv.line(
         frame,
         (int(landmarkA.x), int(landmarkA.y)),
@@ -31,6 +31,8 @@ def drawLine(landmarkA, landmarkB, frame, wideBoyFactor, hex="FFFFFF"):
             )
         ),
     )
+
+# def estimate_distance()
 
 def estimate_pose(cam_or_vid: str):
     blackBg = np.zeros((HEIGHT, WIDTH, 3), dtype = np.uint8)
@@ -48,8 +50,13 @@ def estimate_pose(cam_or_vid: str):
         # Get frame from vid or webcam
         _, frame = cap.read()
 
+        # Resize captured frame to preset values
         frame = cv.resize(frame, (WIDTH, HEIGHT))
 
+        # Flip image to reflect what is on the screen directly
+        frame = cv.flip(frame, 1)
+
+        # Create a black frame to later draw extrapolated pose on
         cv.rectangle(blackBg, (0, 0), (WIDTH-1, HEIGHT-1), (0, 0, 0), -1)
 
         # To improve performance, optionally mark the image as not writeable to
@@ -62,121 +69,156 @@ def estimate_pose(cam_or_vid: str):
         frame.flags.writeable = True
         frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
 
-        for landmark in results.pose_landmarks.landmark:
-            landmark.x, landmark.y = landmark.x * WIDTH, landmark.y * HEIGHT
+        try:
+            for landmark in results.pose_landmarks.landmark:
+                landmark.x, landmark.y = landmark.x * WIDTH, landmark.y * HEIGHT
 
-        leftEyeInner = results.pose_landmarks.landmark[1]
-        leftMouth = results.pose_landmarks.landmark[9]
-        leftShoulder = results.pose_landmarks.landmark[11]
-        leftElbow = results.pose_landmarks.landmark[13]
-        leftWrist = results.pose_landmarks.landmark[15]
-        leftHip = results.pose_landmarks.landmark[23]
-        leftKnee = results.pose_landmarks.landmark[25]
-        leftAnkle = results.pose_landmarks.landmark[27]
+            leftEyeInner = results.pose_landmarks.landmark[1]
+            leftMouth = results.pose_landmarks.landmark[9]
+            leftShoulder = results.pose_landmarks.landmark[11]
+            leftElbow = results.pose_landmarks.landmark[13]
+            leftWrist = results.pose_landmarks.landmark[15]
+            leftPinky = results.pose_landmarks.landmark[17]
+            leftIndex = results.pose_landmarks.landmark[19]
+            leftThumb = results.pose_landmarks.landmark[21]
+            leftHip = results.pose_landmarks.landmark[23]
+            leftKnee = results.pose_landmarks.landmark[25]
+            leftAnkle = results.pose_landmarks.landmark[27]
 
-        rightEyeInner = results.pose_landmarks.landmark[4]
-        rightMouth = results.pose_landmarks.landmark[10]
-        rightShoulder = results.pose_landmarks.landmark[12]
-        rightElbow = results.pose_landmarks.landmark[14]
-        rightWrist = results.pose_landmarks.landmark[16]
-        rightHip = results.pose_landmarks.landmark[24]
-        rightKnee = results.pose_landmarks.landmark[26]
-        rightAnkle = results.pose_landmarks.landmark[28]
+            rightEyeInner = results.pose_landmarks.landmark[4]
+            rightMouth = results.pose_landmarks.landmark[10]
+            rightShoulder = results.pose_landmarks.landmark[12]
+            rightElbow = results.pose_landmarks.landmark[14]
+            rightWrist = results.pose_landmarks.landmark[16]
+            rightPinky = results.pose_landmarks.landmark[18]
+            rightIndex = results.pose_landmarks.landmark[20]
+            rightThumb = results.pose_landmarks.landmark[22]
+            rightHip = results.pose_landmarks.landmark[24]
+            rightKnee = results.pose_landmarks.landmark[26]
+            rightAnkle = results.pose_landmarks.landmark[28]
 
-        chestPts = np.array(
-            [
-                [rightShoulder.x, rightShoulder.y],
-                [leftShoulder.x, leftShoulder.y],
-                [leftHip.x, leftHip.y],
-                [rightHip.x, rightHip.y],
-            ],
-            np.int32,
-        )
-
-        chestPts = chestPts.reshape((-1, 1, 2))
-
-        neckPts = np.array(
-            [
-                [rightShoulder.x, rightShoulder.y],
-                [leftShoulder.x, leftShoulder.y],
-                [leftMouth.x, leftMouth.y],
-                [rightMouth.x, rightMouth.y],
-            ],
-            np.int32,
-        )
-
-        neckPts = neckPts.reshape((-1, 1, 2))
-
-        # Calculate the FPS of the output video
-        cTime = time.time()
-        fps = 1 / (cTime - previous_frame_time)
-        previous_frame_time = cTime
-
-        cv.putText(
-            blackBg, str(int(fps)), (70, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3
-        )
-
-        # Fill neck and chest based on landmarks
-        cv.fillPoly(blackBg, [chestPts], color=(255, 255, 255))
-        cv.fillPoly(blackBg, [neckPts], color=(255, 255, 255))
-
-        # Elipse drawing over the head based on the middle point between eyes
-        cv.ellipse(
-            blackBg,
-            (int((rightEyeInner.x + leftEyeInner.x)/2),
-               int((rightEyeInner.y + leftEyeInner.y)/2)),
-              (int(pythagoras_normalized(rightShoulder, leftShoulder)*0.3),
-               int(pythagoras_normalized(rightShoulder, leftShoulder)*0.45)),
-               0,
-               0,
-               360,
-               (255, 255, 255),
-               -1
+            chestPts = np.array(
+                [
+                    [rightShoulder.x, rightShoulder.y],
+                    [leftShoulder.x, leftShoulder.y],
+                    [leftHip.x, leftHip.y],
+                    [rightHip.x, rightHip.y],
+                ],
+                np.int32,
             )
 
-        # shoulders
-        drawLine(leftShoulder, rightShoulder, blackBg, 7)
+            chestPts = chestPts.reshape((-1, 1, 2))
 
-        # hips
-        drawLine(leftHip, rightHip, blackBg, 7)
+            neckPts = np.array(
+                [
+                    [rightShoulder.x, rightShoulder.y],
+                    [leftShoulder.x, leftShoulder.y],
+                    [leftMouth.x, leftMouth.y],
+                    [rightMouth.x, rightMouth.y],
+                ],
+                np.int32,
+            )
 
-        # left mouth > left shoulder
-        drawLine(leftMouth, leftShoulder, blackBg, 8 )
+            neckPts = neckPts.reshape((-1, 1, 2))
 
-        # left shoulder > left elbow
-        drawLine(leftShoulder, leftElbow, blackBg, 3.8)
+            # Calculate the FPS of the output video
+            cTime = time.time()
+            fps = 1 / (cTime - previous_frame_time)
+            previous_frame_time = cTime
 
-        # left elbow > left wrist
-        drawLine(leftElbow, leftWrist, blackBg, 5)
+            # Print FPS for monitoring on the pose extrapolated frame
+            cv.putText(
+                blackBg, "fps: " + str(int(fps)), (70, 50), cv.FONT_HERSHEY_PLAIN, 3, (3, 252, 177), 3
+            )
+            cv.putText(
+                blackBg, "shoulders_dist: " + str(int(pythagoras_normalized(leftShoulder, rightShoulder))), (1000, 50), cv.FONT_HERSHEY_PLAIN, 1, (252, 165, 3), 1
+                )
+            cv.putText(
+            blackBg, "hips_dist: " + str(int(pythagoras_normalized(leftHip, rightHip))), (1000, 80), cv.FONT_HERSHEY_PLAIN, 1, (252, 165, 3), 1
+            )
+            cv.putText(
+                blackBg, "left_side_dist: " + str(int(pythagoras_normalized(leftShoulder, leftHip))), (1000, 110), cv.FONT_HERSHEY_PLAIN, 1, (252, 165, 3), 1
+                )
+            cv.putText(
+                blackBg, "right_side_dist: " + str(int(pythagoras_normalized(leftShoulder, rightHip))), (1000, 140), cv.FONT_HERSHEY_PLAIN, 1, (252, 165, 3), 1
+                )
 
-        # left hip > left knee
-        drawLine(leftHip, leftKnee, blackBg, 3)
+            # Fill neck and chest based on landmarks
+            cv.fillPoly(blackBg, [chestPts], color=(255, 255, 255))
+            cv.fillPoly(blackBg, [neckPts], color=(255, 255, 255))
 
-        # left knee > left ankle
-        drawLine(leftKnee, leftAnkle, blackBg, 3.5)
+            # Elipse drawing over the head based on the middle point between eyes
+            cv.ellipse(
+                blackBg,
+                (int((rightEyeInner.x + leftEyeInner.x)/2),
+                int((rightEyeInner.y + leftEyeInner.y)/2)),
+                (int(pythagoras_normalized(rightShoulder, leftShoulder)*0.3),
+                int(pythagoras_normalized(rightShoulder, leftShoulder)*0.45)),
+                0,
+                0,
+                360,
+                (255, 255, 255),
+                -1
+                )
 
-        # right shoulder > right elbow
-        drawLine(rightShoulder, rightElbow, blackBg, 3.8)
+            # shoulders
+            draw_line(leftShoulder, rightShoulder, blackBg, 6)
 
-        # right elbow > right wrist
-        drawLine(rightElbow, rightWrist, blackBg, 5)
+            # hips
+            draw_line(leftHip, rightHip, blackBg, 6)
 
-        # right shoulder > right hip
-        drawLine(rightShoulder, rightHip, blackBg, 7)
+            # left shoulder > left elbow
+            draw_line(leftShoulder, leftElbow, blackBg, 3.8)
 
-        # right hip > right knee
-        drawLine(rightHip, rightKnee, blackBg, 3)
+            # left elbow > left wrist
+            draw_line(leftElbow, leftWrist, blackBg, 5)
 
-        # right knee > right ankle
-        drawLine(rightKnee, rightAnkle, blackBg, 3.5)
+            # left hand
+            draw_line(leftWrist, leftPinky, blackBg, 8)
+            draw_line(leftWrist, leftIndex, blackBg, 8)
+            draw_line(leftWrist, leftThumb, blackBg, 8)
 
-        cv.imshow("Original feed", frame)
-        cv.imshow("Extrapolated pose", blackBg)
+            # left shoulder > left hip
+            draw_line(leftShoulder, leftHip, blackBg, 6.5)
 
-        # Quit if 'q' is pressed
-        if cv.waitKey(1) & 0xFF == ord("q"):
+            # left hip > left knee
+            draw_line(leftHip, leftKnee, blackBg, 3)
+
+            # left knee > left ankle
+            draw_line(leftKnee, leftAnkle, blackBg, 3.5)
+
+            # right shoulder > right elbow
+            draw_line(rightShoulder, rightElbow, blackBg, 3.8)
+
+            # right elbow > right wrist
+            draw_line(rightElbow, rightWrist, blackBg, 5)
+
+            # right hand
+            draw_line(rightWrist, rightPinky, blackBg, 8)
+            draw_line(rightWrist, rightIndex, blackBg, 8)
+            draw_line(rightWrist, rightThumb, blackBg, 8)
+
+            # right shoulder > right hip
+            draw_line(rightShoulder, rightHip, blackBg, 6.5)
+
+            # right hip > right knee
+            draw_line(rightHip, rightKnee, blackBg, 3)
+
+            # right knee > right ankle
+            draw_line(rightKnee, rightAnkle, blackBg, 3.5)
+
+            cv.imshow("Original feed", frame)
+            cv.moveWindow("Original feed", 0, 0)
+            cv.imshow("Extrapolated pose", blackBg)
+            cv.moveWindow("Extrapolated pose", 1280, 0)
+
+            # Quit if 'q' is pressed
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+        
+        except: 
+            print("Something went wrong... Most likely a landmark used to extrapolate pose has not been recognized. Try rerunning the program and make sure you are in the FOV of your webcamera or that the video you are trying to analyze starts with a frame where landmarks can be captured.")
             break
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
