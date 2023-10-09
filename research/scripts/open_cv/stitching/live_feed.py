90abdfd6dd3eead88e2d4f9bc390596917fb45c0
jendafojtik <johnyfojtik@gmail.com>
2023-10-09 12:31:49 +0200

    argument parsing for stitching, live_feed for setting up added

---
+++ research/scripts/open_cv/stitching/live_feed.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import argparse
+
+parser = argparse.ArgumentParser(description="Capture images from cameras and perform stitching.")
+parser.add_argument("--num_cameras", type=int, default=2, help="Number of cameras connected.")
+args = parser.parse_args()
+
+cameras = [cv.VideoCapture(idx, cv.CAP_DSHOW) for idx in range(args.num_cameras)]
+
+# Check if the cameras are opened successfully
+for idx, camera in enumerate(cameras):
+    if not camera.isOpened():
+        print(f"[INFO]: Could not open camera {idx}.")
+        cameras[idx] = None
+
+while True:
+    for idx, camera in enumerate(cameras):
+        if camera is not None:
+            # Read a frame from the camera
+            ret, frame = camera.read()
+
+            if ret:
+                # Display the frame from the camera
+                cv.imshow(f"camera_{idx}", frame)
+                cv.moveWindow(f"camera_{idx}", idx*650, 0)
+
+    # Exit the loop when the 'q' key is pressed
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release camera resources and close OpenCV windows
+for camera in cameras:
+    if camera is not None:
+        camera.release()
+cv.destroyAllWindows()
--- research/scripts/open_cv/stitching/webcam_stitching_timer.py
+++ research/scripts/open_cv/stitching/webcam_stitching_timer.py
@@ -3,26 +3,35 @@ import os
 import datetime
 import imutils as imu
 import time
+import argparse
 
-# Initialize camera capture for both cameras (0 and 1 typically represent the first and second cameras IF there is no laptop/integrated camera present)
-camera1 = cv.VideoCapture(0, cv.CAP_DSHOW)
-camera2 = cv.VideoCapture(1, cv.CAP_DSHOW)
+# Initialize the argument parser
+parser = argparse.ArgumentParser(description="Capture images from cameras and perform stitching.")
+parser.add_argument("--num_cameras", type=int, default=2, help="Number of cameras connected.")
+parser.add_argument("--builtin_camera", action="store_false", help="Integrated camera present.")
+args = parser.parse_args()
 
-# Check if the cameras are opened successfully
-if not camera1.isOpened() or not camera2.isOpened():
-    print("[INFO]: Could not open cameras.")
+camera_indices = list(range(args.num_cameras))
+if not args.builtin_camera:
+    camera_indices = [idx for idx in camera_indices if idx not in [0, 1]] # Remove integrated cameras
+
+# Initialize camera captures for indexed cameras
+cameras = [cv.VideoCapture(idx, cv.CAP_DSHOW) for idx in camera_indices]
+
+# Check if indexed cameras were opened correctly
+if not all(camera.isOpened() for camera in cameras):
+    print("[INFO] Could not open cameras.")
     exit()
 
 # Capture a single frame from each camera
-ret1, frame1 = camera1.read()
-ret2, frame2 = camera2.read()
+frames = [camera.read()[1] for camera in cameras]
 
 # Release camera resources
-camera1.release()
-camera2.release()
+for camera in cameras:
+    camera.release()
 
-print("[INFO]: Loading images...")
-if ret1 and ret2:
+print("[INFO] Loading images...")
+if all(frame is not None for frame in frames):
     # Create a directory with the current date and time as the name
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     save_directory = os.path.join("docs", "stitching", current_time)
@@ -31,27 +40,27 @@ if ret1 and ret2:
     os.makedirs(save_directory, exist_ok=True)
 
     # Save the captured frames in the directory
-    cv.imwrite(os.path.join(save_directory, "camera_left.jpg"), frame1)
-    cv.imwrite(os.path.join(save_directory, "camera_right.jpg"), frame2)
+    for i, frame in enumerate(frames):
+        cv.imwrite(os.path.join(save_directory, f"camera_{i+1}.jpg"), frame)
 
-    cv.imshow("camera_left", frame1)
-    cv.imshow("camera_right", frame2)
+    cv.imshow("camera_left", frames[0])
+    cv.imshow("camera_right", frames[1])
 
-    print(f"[INFO]: Images saved in directory: {save_directory}")
+    print(f"[INFO] Images saved in directory: {save_directory}")
 
     # Perform image stitching
-    print("[INFO]: Stitching images...")
+    print("[INFO] Stitching images...")
     stitcher = cv.createStitcher() if imu.is_cv3() else cv.Stitcher_create()
 
     start_time = time.time()
-    (status, stitched) = stitcher.stitch([frame1, frame2])
+    (status, stitched) = stitcher.stitch(frames)
     end_time = time.time()
 
     if status == cv.Stitcher_OK:
         # Save the stitched image to the "docs/stitching" directory
         stitch_filename = os.path.join(save_directory, f"{current_time}_stitched.jpg")
         cv.imwrite(stitch_filename, stitched)
-        print(f"[INFO]: Stitched image saved as {stitch_filename}")
+        print(f"[INFO] Stitched image saved as {stitch_filename}")
  
         # Display the stitched image
         cv.imshow("Stitched image", stitched)
@@ -71,7 +80,7 @@ if ret1 and ret2:
             with open(text_filename, "w") as text_file:
                 text_file.write(f"Distance: 25cm\n")
                 text_file.write(f"View: forward with camera slanted down\n")
-        print("[INFO]: Image stitching failed ({})".format(status))
+        print("[INFO] Image stitching failed ({})".format(status))
 
         # Rename the directory to indicate the failure
         os.rename(save_directory, save_directory + " - fail")
@@ -79,4 +88,4 @@ if ret1 and ret2:
     cv.waitKey(0)
     cv.destroyAllWindows()
 else:
-    print("[INFO]: Failed to capture frames from one or both cameras.")
+    print("[INFO] Failed to capture frames from one or both cameras.")
