7e1656f22dbd01c4eda52abd9efdc2640c79486c
jendafojtik <jan.fojtik@indicia.nl>
2023-11-01 11:34:32 +0100

    first version of the composite graph generation

---
+++ research/scripts/mediapipe/depth_estimation_composite_graph.py
@@ -0,0 +1,127 @@
+import cv2 as cv
+import mediapipe as mp
+import numpy as np
+import os
+import datetime
+import argparse
+import re
+import matplotlib
+
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import json
+
+parser = argparse.ArgumentParser(
+    description="Script to process images and add pose information and generate composite graph."
+)
+
+parser.add_argument(
+    "--dir",
+    type=str,
+    default="default_name",
+    help="Directory from which to read images.",
+)
+
+args = parser.parse_args()
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+
+all_distances_cm = []
+all_distances_px = []
+plot_colors = [
+    "black",
+    "blue",
+    "red",
+    "green",
+    "purple",
+    "orange",
+    "pink",
+    "brown",
+    "gray",
+]
+
+parent_directory = "../../sauce/" + args.dir  # final_location | teacher_table_location
+
+child_dirs = os.listdir(parent_directory)
+
+# List child_dirs in the parent_dir
+child_dirs = [
+    d
+    for d in os.listdir(parent_directory)
+    if os.path.isdir(os.path.join(parent_directory, d))
+]
+
+with mpPose.Pose(static_image_mode=True, model_complexity=2) as pose:
+    # For each child_dir in parent_dirs
+    for child_dir in child_dirs:
+        child_dir_path = os.path.join(parent_directory, child_dir)
+        # Get all image files in child_dir
+        image_files = [
+            f
+            for f in os.listdir(child_dir_path)
+            if any(f.lower().endswith(ext) for ext in ".jpg")
+        ]
+
+        distances_cm = []
+        distances_px = []
+
+        # For all images in child_dir
+        for image_name in image_files:
+            image_path = os.path.join(child_dir_path, image_name)
+            img = cv.imread(image_path)
+            imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+            results = pose.process(imgRGB)
+
+            if results.pose_landmarks:
+                landmark = results.pose_landmarks.landmark
+                mpDraw.draw_landmarks(
+                    img, results.pose_landmarks, mpPose.POSE_CONNECTIONS
+                )
+
+                shoulder_landmark = mpPose.PoseLandmark.RIGHT_SHOULDER
+                nose_landmark = mpPose.PoseLandmark.NOSE
+
+                shoulder_coords = (
+                    int((landmark[12].x + landmark[11].x) * img.shape[1] / 2),
+                    int((landmark[12].y + landmark[11].y) * img.shape[0] / 2),
+                )
+
+                nose_coords = (
+                    int(landmark[0].x * img.shape[1]),
+                    int(landmark[0].y * img.shape[0]),
+                )
+
+                distance_cm = (
+                    int(re.findall(r"\d+", image_name)[0])
+                    if re.findall(r"\d+", image_name)
+                    else 0
+                )
+
+                distance_px = np.sqrt(
+                    (nose_coords[0] - shoulder_coords[0]) ** 2
+                    + (nose_coords[1] - shoulder_coords[1]) ** 2
+                )
+
+            distances_cm.append(distance_cm)
+            distances_px.append(distance_px)
+
+        all_distances_cm.append(distances_cm)
+        all_distances_px.append(distances_px)
+
+for i in range(len(child_dirs)):
+    color_index = i % len(plot_colors)
+    plt.plot(
+        all_distances_cm[i],
+        all_distances_px[i],
+        "-",
+        markersize=0.75,
+        label=f"Distances for {child_dirs[i]}",
+        color=plot_colors[color_index],
+    )
+
+plt.title("Pixel Distance vs. Frames")
+plt.xlabel("Distance (cm)")
+plt.ylabel("Distance (px)")
+plt.legend()
+plt.show()
--- research/scripts/mediapipe/depth_estimation_image.py
+++ research/scripts/mediapipe/depth_estimation_image.py
@@ -1,4 +1,3 @@
-import math
 import cv2 as cv
 import mediapipe as mp
 import numpy as np
@@ -7,13 +6,21 @@ import datetime
 import argparse
 import re
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import json
 
-parser = argparse.ArgumentParser(description="Script to process images and add pose information.")
+parser = argparse.ArgumentParser(
+    description="Script to process images and add pose information."
+)
 
-parser.add_argument("--dir", type=str, default="default_name", help="Directory from and to which to read and write images.")
+parser.add_argument(
+    "--dir",
+    type=str,
+    default="default_name",
+    help="Directory from and to which to read and write images.",
+)
 parser.add_argument("--model_complexity", type=int, default=1, help="Model complexity.")
 parser.add_argument("--static_image", type=bool, default=True, help="Static mode.")
 
@@ -24,7 +31,7 @@ mpPose = mp.solutions.pose
 
 distances_cm = []
 distances_px = []
- 
+
 directory = "../../sauce/" + args.dir
 images = []
 
@@ -32,15 +39,16 @@ files = os.listdir(directory)
 
 images = [file for file in files if file.lower().endswith(".jpg")]
 
-current_date = datetime.datetime.now().strftime("../results/%Y-%m-%d_%H-%M-%S[" + args.dir + "]")
-output_directory = os.path.join("../../results", current_date)
+current_date = datetime.datetime.now().strftime(
+    "../results/%Y-%m-%d_%H-%M-%S[" + args.dir + "]"
+)
+output_directory = os.path.join("../../results/", current_date)
 os.makedirs(output_directory, exist_ok=True)
 
 with mpPose.Pose(
-       static_image_mode=args.static_image,
-       model_complexity=args.model_complexity) as pose:
+    static_image_mode=args.static_image, model_complexity=args.model_complexity
+) as pose:
     for image_name in images:
-
         img = cv.imread(os.path.join(directory, image_name))
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         results = pose.process(imgRGB)
@@ -64,11 +72,26 @@ with mpPose.Pose(
 
             cv.line(img, shoulder_coords, nose_coords, (0, 255, 0), 3)
 
-            distance_px = np.sqrt((nose_coords[0] - shoulder_coords[0]) ** 2 + (nose_coords[1] - shoulder_coords[1]) ** 2)
-            
-            distance_cm = int(re.findall(r'\d+', image_name)[0]) if re.findall(r'\d+', image_name) else 0
-            
-            cv.putText(img, str(int(distance_px)), (300, 300), cv.FONT_HERSHEY_PLAIN, 20, (0, 255, 0), 20)
+            distance_px = np.sqrt(
+                (nose_coords[0] - shoulder_coords[0]) ** 2
+                + (nose_coords[1] - shoulder_coords[1]) ** 2
+            )
+
+            distance_cm = (
+                int(re.findall(r"\d+", image_name)[0])
+                if re.findall(r"\d+", image_name)
+                else 0
+            )
+
+            cv.putText(
+                img,
+                str(int(distance_px)),
+                (300, 300),
+                cv.FONT_HERSHEY_PLAIN,
+                20,
+                (0, 255, 0),
+                20,
+            )
 
         distances_cm.append(distance_cm)
         distances_px.append(distance_px)
@@ -80,14 +103,14 @@ with mpPose.Pose(
         height = 1440
         dimensions = (width, height)
 
-        img = cv.resize(img, dimensions, interpolation= cv.INTER_LINEAR)
+        img = cv.resize(img, dimensions, interpolation=cv.INTER_LINEAR)
         # cv.imshow(image_name, img)
 
-plt.plot(distances_cm, distances_px, marker='o')
+plt.plot(distances_cm, distances_px, marker="o")
 
-plt.title('Pixel Distance vs. Distance in Centimeters')
-plt.xlabel('Distance (cm)')
-plt.ylabel('Pixel Distance')
+plt.title("Pixel Distance vs. Distance in Centimeters")
+plt.xlabel("Distance (cm)")
+plt.ylabel("Pixel Distance")
 
 plot_output_path = os.path.join(output_directory, "aa_plot_image.png")
 plt.savefig(plot_output_path)
@@ -95,16 +118,10 @@ plt.savefig(plot_output_path)
 data = {
     "pose_data": {
         "model_complexity": args.model_complexity,
-        "static_image_mode": args.static_image
+        "static_image_mode": args.static_image,
     },
-    "image_data": {
-        "width": width,
-        "height": height
-    },
-    "data": {
-        "distances_cm": distances_cm,
-        "distances_px": distances_px
-    }
+    "image_data": {"width": width, "height": height},
+    "data": {"distances_cm": distances_cm, "distances_px": distances_px},
 }
 
 file_path = os.path.join(output_directory, "ab_config_data_log.json")
