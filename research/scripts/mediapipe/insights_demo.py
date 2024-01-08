1a2bdb5653c83dca9b5c57876849b665e27cdf82
Tara <tarawillink@coffeeit.nl>
2024-01-08 13:30:47 +0100

    refactor: slight improvement to formatting

--- research/scripts/mediapipe/insights_demo.py
+++ research/scripts/mediapipe/insights_demo.py
@@ -8,8 +8,7 @@ height = 480
 
 
 def calculate_hue_angle(x, y):
-    angle = math.degrees(math.atan2(y, x))
-    return (angle + 180) % 360
+    return (np.degrees(np.arctan2(y, x)) + 180) % 360
 
 
 def interpolate_color(angle):
@@ -24,39 +23,28 @@ def interpolate_color(angle):
 
 def draw_palm_box(frame, palm_landmarks, scale_factor=1, output_size=(20, 20), upscale_factor=5):
 
-    # Create a black background image with the same size as the input frame
     black_frame = np.zeros_like(frame)
 
-    if not palm_landmarks:
-        # Resize the black frame to the desired output size
-        black_frame_resized = cv2.resize(black_frame, output_size, interpolation=cv2.INTER_AREA)
+    if palm_landmarks:
+        # Get coordinates of specific landmarks
+        landmarks_indices = [0, 1, 2, 5, 9, 13, 17]
+        landmarks_coordinates = np.array(
+            [(palm_landmarks[i].x * width, palm_landmarks[i].y * height) for i in landmarks_indices], dtype=np.int32)
 
-        # Upscale the image
-        upscaled_frame = cv2.resize(black_frame_resized,
-                                    (output_size[0] * upscale_factor, output_size[1] * upscale_factor),
-                                    interpolation=cv2.INTER_NEAREST)
+        # Calculate bounding box parameters and apply the scale factor
+        x, y, w, h = cv2.boundingRect(landmarks_coordinates)
+        w *= scale_factor
+        h *= scale_factor
 
-        return upscaled_frame
+        # Calculate hue based on hand orientation
+        hue_angle = calculate_hue_angle(palm_landmarks[9].x - palm_landmarks[5].x,
+                                        palm_landmarks[9].y - palm_landmarks[5].y)
 
-    # Get coordinates of specific landmarks
-    landmarks_indices = [0, 1, 2, 5, 9, 13, 17]
-    landmarks_coordinates = np.array(
-        [(palm_landmarks[i].x * width, palm_landmarks[i].y * height) for i in landmarks_indices], dtype=np.int32)
+        # Interpolate color based on hue angle
+        color = interpolate_color(hue_angle)
 
-    # Calculate bounding box parameters and apply the scale factor
-    x, y, w, h = cv2.boundingRect(landmarks_coordinates)
-    w *= scale_factor
-    h *= scale_factor
-
-    # Calculate hue based on hand orientation
-    hue_angle = calculate_hue_angle(palm_landmarks[9].x - palm_landmarks[5].x,
-                                    palm_landmarks[9].y - palm_landmarks[5].y)
-
-    # Interpolate color based on hue angle
-    color = interpolate_color(hue_angle)
-
-    # Draw a filled oval (ellipse) around the center of the palm on the black background
-    cv2.ellipse(black_frame, ((x + w // 2, y + h // 2), (w, h), 0), color, -1)
+        # Draw a filled oval (ellipse) around the center of the palm on the black background
+        cv2.ellipse(black_frame, ((x + w // 2, y + h // 2), (w, h), 0), color, -1)
 
     # Resize the black frame to the desired output size
     black_frame_resized = cv2.resize(black_frame, output_size, interpolation=cv2.INTER_AREA)
