3cadd9bc17516299e317defa4f7d5b14211ca390
@I508987 <bas.bakens@live.nl>
2024-04-15 14:39:56 +0200

    Update constructor function naming + Make baudrate variable

--- lib/wrappers/python-new/contourwall.py
+++ lib/wrappers/python-new/contourwall.py
@@ -32,34 +32,39 @@ class ContourWall:
         self._single_new_with_port.argtypes = [c_char_p, c_uint32]
         self._single_new_with_port.restype = ContourWallCore
 
-        self.show = self.__lib.show
-        self.show.argtypes = [POINTER(ContourWallCore)]
-        # self.show.restype = ???
+        self._show = self.__lib.show
+        self._show.argtypes = [POINTER(ContourWallCore)]
+        # self._show.restype = ???
 
-        self.update_all = self.__lib.update_all
-        self.update_all.argtypes = [POINTER(ContourWallCore), POINTER(c_uint8)]
-        # self.update_all.restype = ???
+        self._update_all = self.__lib.update_all
+        self._update_all.argtypes = [POINTER(ContourWallCore), POINTER(c_uint8)]
+        # self._update_all.restype = ???
 
-        self.solid_color = self.__lib.solid_color
-        self.solid_color.argtypes = [POINTER(ContourWallCore), c_uint8, c_uint8, c_uint8]
-        # self.solid_color.restype = ???
+        self._solid_color = self.__lib.solid_color
+        self._solid_color.argtypes = [POINTER(ContourWallCore), c_uint8, c_uint8, c_uint8]
+        # self._solid_color.restype = ???
 
-        self.drop = self.__lib.drop
-        self.drop.argtypes = [POINTER(ContourWallCore)]
-        # self.drop.restype = ???
+        self._drop = self.__lib.drop
+        self._drop.argtypes = [POINTER(ContourWallCore)]
+        # self._drop.restype = ???
 
-        self.pixels: np.array = np.zeros((20, 20, 3), dtype=np.uint8)
+        self._pixels: np.array = np.zeros((20, 20, 3), dtype=np.uint8)
         self.pushed_frames: int = 0
 
-    def new_with_ports(self, port1: str, port2: str, port3: str, port4: str, port5: str, port6: str):
+    def new(self, baudrate=2_000_000):
+        """Create a new instance of ContourWallCore with 0 tiles"""
+
+        self._cw_core = self._new(baudrate)
+
+    def new_with_ports(self, port1: str, port2: str, port3: str, port4: str, port5: str, port6: str, baudrate=2_000_000):
         """Create a new instance of ContourWallCore with 6 tiles"""
 
-        self._cw_core = self._new_with_ports(port1.encode(), port2.encode(), port3.encode(), port4.encode(), port5.encode(), port6.encode(), 2_000_000)
+        self._cw_core = self._new_with_ports(port1.encode(), port2.encode(), port3.encode(), port4.encode(), port5.encode(), port6.encode(), baudrate)
 
-    def single_new_with_port(self, port: str):
+    def single_new_with_port(self, port: str, baudrate=2_000_000):
         """Create a new instance of ContourWallCore with 1 tile"""
 
-        self._cw_core = self._single_new_with_port(port, 2_000_000)
+        self._cw_core = self._single_new_with_port(port.encode(), baudrate)
 
     def show(self):
         """Update each single LED on the ContourWallCore with the pixel data in 'cw.pixels'.
@@ -70,8 +75,9 @@ class ContourWall:
 
         cw.show()"""
 
-        self.update_all(self._cw_core, self.pixels.data_as(POINTER(c_uint8)))
-        self.show(self._cw_core)
+        # check if .data_as(POINTER(c_uint8)) is a possible solution
+        self._update_all(self._cw_core, self._pixels.data_as(POINTER(c_uint8)))
+        self._show(self._cw_core)
 
     def fill_solid(self, r: int, g: int, b: int):
         """fill_solid is a function that fills the entire ContourWall with a single color. Each seperate LED will have the same color.
@@ -79,10 +85,10 @@ class ContourWall:
         Example code to make the entire ContourWall red: 
         
         cw.fill_solid(255, 0, 0)"""
-        self.solid_color(self._cw_core, r, g, b)
+        self._solid_color(self._cw_core, r, g, b)
 
     def drop(self):
         """Drop the ContourWallCore instance"""
 
-        self.drop(self._cw_core)
+        self._drop(self._cw_core)
     
